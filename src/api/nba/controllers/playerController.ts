3116afc74d8264efcb30929654726a904bbbabf9
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2025-11-19 14:04:06 -0600

    Added controller definition for playerController, will define later.

--- src/api/nba/controllers/playerController.ts
+++ src/api/nba/controllers/playerController.ts
@@ -1,31 +1,61 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { HTTP_STATUS } from "../constants/httpConstants";
-import { successResponse, errorResponse } from "../models/responseModel";
-import { Player } from "../models/people/playerModel";
+import { successResponse } from "../models/responseModel";
+// import { Player } from "../models/people/playerModel";
 
 
-export const createPlayer = async (req: Request, res: Response): Promise<void> => {
-
-        // POST nba/player/
+export const createPlayer = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        res.status(HTTP_STATUS.CREATED).json(
+            successResponse({}, "Player created")
+        );
+    }
+    catch (error: unknown) {
+        next(error);
+    }
 };
 
 
-export const getAllPlayers = async (req: Request, res: Response): Promise<void> => {
-    
-    // GET nba/player/
+export const getAllPlayers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        res.status(HTTP_STATUS.OK).json(
+            successResponse({}, "Player created")
+        );
+    }
+    catch (error: unknown) {
+        next(error);
+    }
 };
 
-export const getPlayerById = async (req: Request, res: Response): Promise<void> => {
-    
-    // GET nba/player/:id
+export const getPlayerById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        res.status(HTTP_STATUS.OK).json(
+            successResponse({}, "Player created")
+        );
+    }
+    catch (error: unknown) {
+        next(error);
+    }
 };
 
-export const reviewPlayer = async (req: Request, res: Response): Promise<void> => {
-
-        // PUT nba/player/review/:id
+export const reviewPlayer = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        res.status(HTTP_STATUS.OK).json(
+            successResponse({}, "Player created")
+        );
+    }
+    catch (error: unknown) {
+        next(error);
+    }
 };
 
-export const updatePlayer = async (req: Request, res: Response): Promise<void> => {
-
-        // PUT nba/player/status/:id
+export const updatePlayer = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        res.status(HTTP_STATUS.OK).json(
+            successResponse({}, "Player created")
+        );
+    }
+    catch (error: unknown) {
+        next(error);
+    }
 };
