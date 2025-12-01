75d307fe474a2685730a495d08e90f6b447b3c8d
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2025-11-30 18:02:19 -0600

    Added Coach tests.

--- test/controllers/coachController.test.ts
+++ test/controllers/coachController.test.ts
@@ -1,102 +1,125 @@
-import * as firestoreRepository from '../../src/api/nba/repositories/firestoreRepositories';
+import { NextFunction, Request, Response } from "express";
+import { HTTP_STATUS } from "../../src/api/nba/constants/httpConstants";
+import * as coachController from "../../src/api/nba/controllers/coachController";
 import * as coachService from "../../src/api/nba/services/coachService";
 import { Coach } from '../../src/api/nba/models/people/coachModel';
 
-jest.mock('../../src/api/nba/repositories/firestoreRepositories');
+jest.mock("../../src/api/nba/services/coachService");
 
-describe("coachService", () => {
+describe("Coach Controller", () => {
+    let mockReq: Partial<Request>;
+    let mockRes: Partial<Response>;
+    let mockNext: NextFunction;
+    const mockDate = new Date();
+
+
     beforeEach(() => {
         jest.clearAllMocks();
+        mockReq = { params: {}, body: {} };
+        mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+        mockNext = jest.fn();
     });
 
-    it("should create a Coach with valid arguments", async () => {
-        const mockInput = {
-            name: "Phil Jackson",
-            currentTeam: "Cancun Sharks"
-        };
+    describe("createCoach", () => {
 
-        const result = await coachService.createCoach(mockInput);
+        it("Should return the correct HTTP status code and message with a valid input.", async () => {
 
-        expect(firestoreRepository.createDocument).toHaveBeenCalledWith("coaches", expect.objectContaining({
-            name: "Phil Jackson",
-            currentTeam: "Cancun Sharks"
-        }));
-
-        expect(result.name).toBe(mockInput.name);
-        expect(result.currentTeam).toBe(mockInput.currentTeam);
-
-
-    });
-
-
-    it("Should retrieve all coaches if exists", async () => {
-        const mockCoach = {
-            id: "13rings",
-            name: "Phil Jackson",
-            currentTeam: "Cancun Sharks"
-        };
-
-        const mockSnapshot = {
-            docs: [
-                {
-                    id: "13rings",
-                    data: () => mockCoach,
-                },
-            ],
-        };
-
-        (firestoreRepository.getDocuments as jest.Mock).mockResolvedValue(mockSnapshot);
-
-        const result = await coachService.getCoaches();
-
-        expect(firestoreRepository.getDocuments).toHaveBeenCalledWith("coaches");
-
-        expect(result).toEqual([mockCoach]);
-
-    });
-
-    it("should update coach if exists with valid fields", async () => {
-        const mockId = "13rings";
-        const mockDate = new Date();
-
-        const mockCoach = {
-            id: mockId,
-            name: "Phil Jackson",
-            currentTeam: "Chicago Bulls",
-            createdAt: mockDate,
-            updatedAt: mockDate,
-        };
-
-        const mockBody: Pick<Coach,
-            "name" |
-            "currentTeam"
-        > = {
-            ...mockCoach,
-            currentTeam: "Los Angeles Lakers"
-
-        };
-
-        (firestoreRepository.updateDocument as jest.Mock).mockResolvedValue(mockCoach);
-        (firestoreRepository.getDocumentById as jest.Mock).mockResolvedValue({
-            id: mockId,
-            data: () => ({
+            const mockBody = {
                 name: "Phil Jackson",
-                currentTeam: "Chicago Bulls",
-                createdAt: mockDate,
-                updatedAt: mockDate,
-            }),
+                currentTeam: "Cancun Sharks"
+            };
+
+            mockReq.body = mockBody;
+            (coachService.createCoach as jest.Mock).mockReturnValue(mockBody);
+
+            await coachController.createCoach(mockReq as Request, mockRes as Response, mockNext as NextFunction);
+
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.CREATED);
+            expect(mockRes.json).toHaveBeenCalledWith( expect.objectContaining({
+                status: "Success",
+                message: "Coach created.",
+            }));
+
         });
 
+    });
 
-        const result = await coachService.updateCoach(mockId, mockBody);
+    describe("getCoaches", () => {
 
-        expect(firestoreRepository.updateDocument).toHaveBeenCalledWith("coaches", mockId, expect.objectContaining({
-            currentTeam: "Los Angeles Lakers"
-        }));
+        it("Should return correct HTTP status code and message when there are coaches.", async () => {
+            const mockCoaches: Coach[] = [
+                {
+                    id: "1",
+                    name: "Phil Jackson",
+                    currentTeam: "Cancun Sharks",
+                    createdAt: mockDate,
+                    updatedAt: mockDate
+                }
+            ];
 
-        expect(result.currentTeam).toBe(mockBody.currentTeam);
+            (coachService.getCoaches as jest.Mock).mockReturnValue(mockCoaches);
 
+            await coachController.getCoaches(mockReq as Request, mockRes as Response, mockNext as NextFunction);
+
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.OK);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                status: "Success",
+                data: mockCoaches,
+                message: "Coaches found and returned.",
+            });
+        });
+    });
+
+    describe("getCoachById", () => {
+        it("Return correct HTTP status code and data when coach exists", async () => {
+            const expectedCoach: Coach = {
+                id: "1",
+                name: "Phil Jackson",
+                currentTeam: "Cancun Sharks",
+                createdAt: mockDate,
+                updatedAt: mockDate
+            };
+
+            (coachService.getCoachById as jest.Mock).mockReturnValue(expectedCoach);
+
+            await coachController.getCoachById(mockReq as Request, mockRes as Response, mockNext as NextFunction);
+
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.OK);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                status: "Success",
+                data: expectedCoach,
+                message: "Coach found",
+            });
+        });
+
+    });
+
+    describe("updateCoach", () => {
+        it("Returned the correct HTTP status code and data when a coach is updated", async () => {
+            const expectedCoach: Coach = {
+                id: "1",
+                name: "Phil Jackson",
+                currentTeam: "Cancun Sharks",
+                createdAt: mockDate,
+                updatedAt: mockDate
+            };
+
+            (coachService.updateCoach as jest.Mock).mockReturnValue(expectedCoach);
+
+            await coachController.updateCoach(mockReq as Request, mockRes as Response, mockNext as NextFunction);
+
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.OK);
+            expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
+                status: "Success",
+                message: "Coach updated",
+            }));
+
+        });
 
     });
 
 });
+
