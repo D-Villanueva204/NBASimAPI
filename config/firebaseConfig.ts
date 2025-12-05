ee7d5fff666e921179093ef26c80b3e711576d1c
dvillanueva2 <dvillanueva2@academic.rrc.ca>
2025-12-04 23:00:43 -0600

    Added all middleware and controllers for role based authentication.

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,7 +1,8 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
+import { getAuth, Auth } from "firebase-admin/auth";
 
-// You'll n eed to replace this with your actual service account file name
+// You'll need to replace this with your actual service account file name
 import serviceAccount from "../capstone-project-75fdd-firebase-adminsdk-fbsvc-313c998d1b.json";
 
 // initialize the Firebase app with our service account key
@@ -9,9 +10,11 @@ initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
 });
 
+const auth: Auth = getAuth();
+
 // get a reference to the firestore database
 const db: Firestore = getFirestore();
 
 db.settings({ ignoreUndefinedProperties: true });
 
-export { db };
+export { auth, db };
